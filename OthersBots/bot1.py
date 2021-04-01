753047da751f04acca04e6bf8b98ec32ce2d8e03
EmersonCob <58399211+EmersonCob@users.noreply.github.com>
2021-04-01 18:42:24 -0300

    updating

---
+++ OthersBots/bot1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import os, telepot, urllib, json
+
+bot = telebot.TeleBot("1566848654:AAEBS9TcwAHm5wh93Z5BtQoi9k5UB44doYg")
+
+getUpdates()
--- bot1.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import os, telepot, urllib, json
-
-bot = telebot.TeleBot("1566848654:AAEBS9TcwAHm5wh93Z5BtQoi9k5UB44doYg")
-
